d8303d25a5eb16dd14d2e513435d6e9de7dc0b74
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-02 15:41:44 +0000

    Set up React-router v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import {Routes, Route} from 'react-router-dom';
+
 function App() {
-  return null;
+  return (
+    <div>
+
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/About' element={<Home/>}/>
+    </Routes>
+    </div>
+  );
 }
 
 export default App;
