8f42fb0a94b2ddfe4635a7f913746723f2fdd0ee
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-03 01:29:43 +0000

    Update Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+
+
+
+    return (
+        <nav>
+            <ul>
+                <li >test</li>
+                <li>test2</li>
+            </ul>
+        </nav>
+    );
+};
+
+
+
+export default Navbar;
