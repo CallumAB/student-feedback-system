694df91e96595ad1b5c8a73a136abb0731b2ce79
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-03 01:29:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import Home from "./components/Home";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-
+    <Navbar/>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Login/>}/>
       <Route path='/About' element={<Home/>}/>
     </Routes>
     </div>
