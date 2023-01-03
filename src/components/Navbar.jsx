a77f964deee4cfaf3c7c2580d2a846eed5fe73f0
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-03 16:10:41 +0000

    Updated navbar to be responsive
    Added hamburger menu
    - doesn't work yet
    
    Dropdown menu is invisible for some reason

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import './navbar.css';
 
 const Navbar = () => {
-
-
-
     return (
-        <nav>
-            <ul>
-                <li >test</li>
-                <li>test2</li>
-            </ul>
+        <nav class="navbar">
+            <div class="logo-title">Logo</div>
+            <a href='#' class='toggle-button'>
+                <span class='bar'></span>
+                <span class='bar'></span>
+                <span class='bar'></span>
+            </a>
+            <div class="navbar-links">
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Contact</a></li>
+                </ul>
+            </div>
         </nav>
     );
 };
