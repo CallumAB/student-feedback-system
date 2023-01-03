6ad17bb80df9dfd7ccd289e5c6bf88d4403251f0
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-03 01:28:32 +0000

    Created new pages folder and moved home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from '../components/Counter';
+
+const Home = () => {
+    return (
+        <div> 
+            <div>hi</div>
+            <div>help</div>
+            <Counter/>
+        </div>
+    );
+};
+
+export default Home;
