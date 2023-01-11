e479e7e87207e2227a554e0505e0bb3a4578e1e1
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-11 02:32:29 +0000

    Updated routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Login from "./pages/Login";
+import Signup from "./pages/Signup"
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import {Routes, Route} from 'react-router-dom';
+import QuestionCard from "./components/QuestionCard";
 
 function App() {
   return (
     <div>
     <Navbar/>
     <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/About' element={<Home/>}/>
+      <Route path='/' element={<QuestionCard/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/home' element={<Home/>}/>
     </Routes>
     </div>
   );
