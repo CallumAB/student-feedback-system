8b6a9b33c5ba41d2e981cf796e17d6bf43c7e4d7
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-11 02:31:13 +0000

    Set up login and signup with firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,87 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {  signInWithEmailAndPassword   } from 'firebase/auth';
+import { auth } from '../firebase';
+import { NavLink, useNavigate } from 'react-router-dom'
+ 
 const Login = () => {
-
-    const loginStyle = {
-        fontWeight: 'bold',
-        fontSize: '2rem',
-
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+       
+    const onLogin = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            navigate("/home")
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage)
+        });
+       
     }
+ 
+    return(
+        <>
+            <main >        
+                <section>
+                    <div>                                            
+                        <p> Student feedback </p>                       
+                                                       
+                        <form>                                              
+                            <div>
+                                <label htmlFor="email-address">
+                                    Email address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"                                    
+                                    required                                                                                
+                                    placeholder="Email address"
+                                    onChange={(e)=>setEmail(e.target.value)}
+                                />
+                            </div>
 
-    const buttonStyle = {
-        color: '#ffffff',
-        backgroundColor: '#405cf5',
-
-        height: '44px',
-        width: '100%',
-        
-        borderRadius: '6px',
-        borderWidth: '0',
-        boxSizing: 'border-box',
-        cursor: 'pointer',
-        margin: '12px 0 0',
-        outline: 'none',
-        padding: '12px 14px',
-        textAlign: 'center',
-        userSelect: 'none',
-        touchAction: 'manipulation',
-        verticalAlign: 'middle',
-    }
-
-    
-
-    return (
-        <div> 
-            <div style={loginStyle}>Continue using University Account...</div>            
-            <button style={buttonStyle}>Continue with google</button>
-        </div>
-    );
-};
-
-export default Login;
+                            <div>
+                                <label htmlFor="password">
+                                    Password
+                                </label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"                                    
+                                    required                                                                                
+                                    placeholder="Password"
+                                    onChange={(e)=>setPassword(e.target.value)}
+                                />
+                            </div>
+                                                
+                            <div>
+                                <button                                    
+                                    onClick={onLogin}                                        
+                                >      
+                                    Login                                                                  
+                                </button>
+                            </div>                               
+                        </form>
+                       
+                        <p className="text-sm text-white text-center">
+                            No account yet? {' '}
+                            <NavLink to="/signup">
+                                Sign up
+                            </NavLink>
+                        </p>
+                                                   
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
+ 
+export default Login
