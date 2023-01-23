12d579876078dadc11584af3ea8c17d696dab0ba
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-23 00:57:02 +0000

    Remaking navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import './navbar.css';
+import React from 'react'
 
 const Navbar = () => {
+
     return (
-        <nav class="navbar">
-            <div class="logo-title">Logo</div>
-            <a href='#' class='toggle-button'>
-                <span class='bar'></span>
-                <span class='bar'></span>
-                <span class='bar'></span>
-            </a>
-            <div class="navbar-links">
-                <ul>
-                    <li><a href='#'>Home</a></li>
-                    <li><a href='#'>About</a></li>
-                    <li><a href='#'>Contact</a></li>
-                </ul>
-            </div>
-        </nav>
-    );
-};
+        <div>Navbar</div>
+    )
+}
 
-
-
-export default Navbar;
+export default Navbar
