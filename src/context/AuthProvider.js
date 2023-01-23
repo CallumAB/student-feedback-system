73bba19d69c3990d8467f485e7576eed76b6f09f
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-23 00:58:06 +0000

    Attempting to set up user context

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,17 @@
+import { auth } from "../firebase"
+import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
+import AuthContext from "./AuthContext";
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        onAuthStateChanged(auth,(user) => {
+            setUser(user)
+        })
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    );
+};
