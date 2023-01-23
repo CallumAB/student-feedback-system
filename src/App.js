77e6adb64c541d5749205f4e5487b7bc93380574
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-23 00:56:46 +0000

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import Signup from "./pages/Signup"
 import Home from "./pages/Home";
 // import Survey from "./pages/Survey"
 import Survey from "./pages/Survey"
-import Navbar from "./components/Navbar";
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route} from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider"
+import PrimaryButton from "./components/PrimaryButton";
+import SecondaryButton from "./components/SecondaryButton";
+
+
 
 function App() {
+
+  const test = () => {
+    console.log("Works")
+  }
+
   return (
     <div>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/home' element={<Home/>}/>
-      <Route path='/survey' element={<Survey/>}/>/
-    </Routes>
+      <PrimaryButton text="Primary" width="100%" onClick={test}/>
+      <SecondaryButton text="Secondary" width="100%"/>
+      <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/signup' element={<Signup/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/survey' element={<Survey/>}/>/   
+      </Routes>
     </div>
   );
 }
