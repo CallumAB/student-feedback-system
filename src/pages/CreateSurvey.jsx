ab560c83b05f360e50925692f58abe68e4793a2c
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-23 01:00:28 +0000

    Start of create survey page

---
+++ src/pages/CreateSurvey.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateSurvey = () => {
+    return (
+        <div> 
+            <div>hi</div>
+            <div>help</div>
+        </div>
+    );
+};
+
+export default CreateSurvey;
