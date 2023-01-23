07f9f2bfd33e4d8d33529a3bab8dfbe8c0c03a3c
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-23 01:00:05 +0000

    Restructured code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,61 +26,35 @@ const Login = () => {
     }
  
     return(
-        <>
-            <main >        
-                <section>
-                    <div>                                            
-                        <p> Student feedback </p>                       
-                                                       
-                        <form>                                              
-                            <div>
-                                <label htmlFor="email-address">
-                                    Email address
-                                </label>
-                                <input
-                                    id="email-address"
-                                    name="email"
-                                    type="email"                                    
-                                    required                                                                                
-                                    placeholder="Email address"
-                                    onChange={(e)=>setEmail(e.target.value)}
-                                />
-                            </div>
+        <div>                                            
+            <h1> Student feedback </h1>                                                 
+            <form>                                              
+                <div>
+                    <label>Email address </label>
+                    <input
+                        type="email"                                                                                                           
+                        placeholder="Email address"
+                        onChange={(e)=>setEmail(e.target.value)}
+                    />
+                </div>
 
-                            <div>
-                                <label htmlFor="password">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"                                    
-                                    required                                                                                
-                                    placeholder="Password"
-                                    onChange={(e)=>setPassword(e.target.value)}
-                                />
-                            </div>
-                                                
-                            <div>
-                                <button                                    
-                                    onClick={onLogin}                                        
-                                >      
-                                    Login                                                                  
-                                </button>
-                            </div>                               
-                        </form>
-                       
-                        <p className="text-sm text-white text-center">
-                            No account yet? {' '}
-                            <NavLink to="/signup">
-                                Sign up
-                            </NavLink>
-                        </p>
-                                                   
-                    </div>
-                </section>
-            </main>
-        </>
+                <div>
+                    <label>Password </label>
+                    <input
+                        type="password"                                                                          
+                        placeholder="Password"
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button onClick={onLogin}>Login</button>                     
+            </form>
+            
+            <p>
+                No account yet? {' '}
+                <NavLink to="/signup">Sign up</NavLink>
+            </p>                          
+        </div>
     )
 }
  
