1dbca81c2f3a7f91f1b792f3cd713f52113afbfe
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-01-23 20:16:36 +0000

    Added protected routes
    - If users aren't logged in they'll be redirected to the login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Survey from "./pages/Survey"
 import {Routes, Route} from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
 
@@ -11,11 +12,12 @@ function App() {
     <div>
       <AuthProvider>
         <Routes>
-              <Route path='/' element={<Home/>}/>
+              
               <Route path='/login' element={<Login/>}/>
               <Route path='/signup' element={<Signup/>}/>
-              <Route path='/home' element={<Home/>}/>
-              <Route path='/survey' element={<Survey/>}/>
+              <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+              <Route path='/home' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+              <Route path='/survey' element={<ProtectedRoute><Survey/></ProtectedRoute>}/>
         </Routes>
       </AuthProvider>
     </div>
