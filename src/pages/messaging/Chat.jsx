898256acdec9191a9a58940143364e2b95f8af09
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-02-08 20:38:06 +0000

    Create Chat.jsx

---
+++ src/pages/messaging/Chat.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const chat = () => {
+  return (
+    <div>
+        <div>
+            {/* Chat Message Component */}
+        </div>
+
+        <div>
+            {/* Send Message Component */}
+        </div>
+
+    </div>
+  )
+}
+
+export default chat
