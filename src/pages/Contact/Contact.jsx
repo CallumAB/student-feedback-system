80d3ab12fddbddb50116a0aacebd3197cf2d5749
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-02-08 20:35:52 +0000

    Remade contact component - database linked
    - Old contact component required users to be logged in

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,73 +1,43 @@
-import React, { useState } from 'react';
-import ModuleDropdown from './ModuleDropdown'
-import ContactDropdown from './ContactDropdown'
+//React && Firebase
+import React, {useState, useEffect} from 'react';
+import {collection, doc, getDocs} from "firebase/firestore";
+import {db} from "../../firebase"
 
+//Components
+import Dropdown from '../../components/Dropdown';
+import CourseContact from './components/CourseContact';
+import ModuleContact from './components/ModuleContact';
+import GenericContact from './components/GenericContact';
 
 const Contact = () => {
+    //State
+    const [selectedReason, setSelectedReason] = useState('Select Reason');
 
-    const [selectedModule, setSelectedModule] = useState('Module');
-    const [selectedContact, setSelectedContact] = useState('Contact');
+    //Test Data
+    const options = ["Problems with course", "Personal", "Generic Category Two", "Generic Category Three", "Other"]
 
-    // Test Data
 
-    const data = [{
-        id: 1,
-        module: "Distributed Systems and Security",
-        contacts: [
-            {
-                id: 1,
-                name: "Mr. So and So",
-            },
-            {
-                id: 2,
-                name: "Misses So and So",
-            }
-        ]
-    },
-    {
-        id: 2,
-        module: "Web development",
-        contacts: [
-            {
-                id: 1,
-                name: "Michelle",
-            },
-            {
-                id: 2,
-                name: "Callum",
-            }
-        ]
-    },
-    ]
-
-    const moduleClick = (e) => {
-        console.log(e.target.textContent)
-        console.log("Module clicked", e.target.id); 
-        setSelectedModule(e.target.textContent);
-        setSelectedContact("Contact")
+    //Handle clicks
+    const reasonClick = (e) => {
+        setSelectedReason(e.target.textContent);
     }
 
-    const contactClick = (e) => {
-        console.log("Contact clicked", e.target.textContent)
-        setSelectedContact(e.target.textContent)
+    //Element variables
+    let nextOptions;
+    if(selectedReason === "Problems with course") {
+        nextOptions = <CourseContact/>
+    } else {
+        nextOptions = <GenericContact category={selectedReason}/>
     }
 
-    const getContactData = () => {
-        if(selectedModule != "Module"){
-            let result = data.find(item => item.module === selectedModule);
-            return result.contacts;
-        } else {
-            return data[0].contacts
-        }
-    }
-
+    //Return
     return (
-        <div> 
-            <div>This is the contact page</div>
-            <ModuleDropdown data={data} text={selectedModule} buttonClick={moduleClick}/>
-            <ContactDropdown data={getContactData()} text={selectedContact} buttonClick={contactClick}/>
+        <div>
+            <div>What is your reason for contacting?</div>
+            <Dropdown text={selectedReason} items={options} buttonClick={reasonClick}></Dropdown>
+            {nextOptions}
         </div>
-    );
-};
+    )
+}
 
-export default Contact;
+export default Contact
