5a69e397a4dc1b049a94e9fb3eb76a02a3330b1e
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-02-08 20:34:28 +0000

    Updated routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Login from "./pages/Login";
 import Signup from "./pages/Signup"
 import Home from "./pages/Home";
-import Contact from "./pages/Contact/Contact"
+import Contact from "./pages/contact/Contact"
 import Survey from "./pages/Survey"
 import {Routes, Route} from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
@@ -17,7 +17,7 @@ function App() {
               <Route path='/signup' element={<Signup/>}/>    
               <Route path='/' element={<Home/>}/>
               <Route path='/home' element={<Home/>}/>
-              <Route path='/contact' element={<ProtectedRoute><Contact/></ProtectedRoute>}/>       
+              <Route path='/contact' element={<ProtectedRoute><Contact/></ProtectedRoute>}/>   
               <Route path='/survey' element={<ProtectedRoute><Survey/></ProtectedRoute>}/>
         </Routes>
       </AuthProvider>
