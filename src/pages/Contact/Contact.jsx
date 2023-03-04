e939515cf5c9c6d25fb2911840859b6637d592c6
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-03-04 01:33:47 +0000

    Added conditional rendering

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,14 +4,18 @@ import {collection, doc, getDocs} from "firebase/firestore";
 import {db} from "../../firebase"
 
 //Components
-import Dropdown from '../../components/Dropdown';
+import Dropdown from '../../components/dropdown/Dropdown';
 import CourseContact from './components/CourseContact';
 import ModuleContact from './components/ModuleContact';
 import GenericContact from './components/GenericContact';
 
+//CSS
+import './contact.css'
+
 const Contact = () => {
     //State
-    const [selectedReason, setSelectedReason] = useState('Select Reason');
+    const defaultReason = "Select Reason";
+    const [selectedReason, setSelectedReason] = useState(defaultReason);
 
     //Test Data
     const options = ["Problems with course", "Personal", "Generic Category Two", "Generic Category Three", "Other"]
@@ -24,7 +28,9 @@ const Contact = () => {
 
     //Element variables
     let nextOptions;
-    if(selectedReason === "Problems with course") {
+    if(selectedReason === defaultReason) {
+        nextOptions = null;
+    } else if(selectedReason === "Problems with course") {
         nextOptions = <CourseContact/>
     } else {
         nextOptions = <GenericContact category={selectedReason}/>
@@ -32,8 +38,10 @@ const Contact = () => {
 
     //Return
     return (
-        <div>
-            <div>What is your reason for contacting?</div>
+        <div className="contact-page">
+            <h3>Contact</h3>
+            <p>What is your reason for contacting?</p>
+            <p>Select a category:</p>
             <Dropdown text={selectedReason} items={options} buttonClick={reasonClick}></Dropdown>
             {nextOptions}
         </div>
