f9319b132638dfd623a163a55d424cc75cb0d68c
Callum Brennan <116171929+CallumAB@users.noreply.github.com>
2023-03-04 01:33:10 +0000

    Updated navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {  signOut } from "firebase/auth";
+import { auth } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
+import { UserAuth } from '../../context/AuthContext';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+   console.log(auth.currentUser)
+
+    const navigate = useNavigate();
+    const handleLogout = () => {               
+        signOut(auth).then(() => {
+        // Sign-out successful.
+            navigate("/login");
+            console.log("Signed out successfully")
+        }).catch((error) => {
+        // An error happened.
+        });
+    }
+
+    return (
+        <nav>
+            <ul>
+                <Link to="/home"><li>Home</li></Link>
+                <Link to="/contact"><li>Contact</li></Link>
+                <Link to="/survey"><li>Surveys</li></Link>
+                {auth.currentUser != null
+                ? 
+                <div>
+                    <li onClick={handleLogout}>Logout</li>
+                </div>  
+                :   <Link to="/login"><li>Login</li></Link>
+                }
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
